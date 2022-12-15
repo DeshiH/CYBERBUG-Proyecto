c1c7e977718b3a23f1c3c50d1f80045f538599ce
Angel Virrueta <Virrueta@outlook.com>
2022-12-14 18:35:00 -0600

    Se agrega modal de carrito. Se agrega uso de localStorage.

--- store/src/main.js
+++ store/src/main.js
@@ -6,6 +6,18 @@ var productoMolido = document.querySelector("#Molido1");
 var DOMcatalogo = document.querySelector('.listaProductos');
 var DOMitems = document.querySelector('#items');
 var carritoBadge = document.querySelector('#iconoCarrito');
+var DOMcarrito = document.querySelector('#carrito');
+var DOMtotal = document.querySelector('#total');
+var DOMtotalArticulos = document.querySelector('#totalArticulos');
+var DOMbotonVaciar = document.querySelector('#vaciarButton');
+var statusCarrito = document.querySelector('#statusCarrito');
+var carrito = [];
+var divisa = '$';
+var contadorItems = 1;
+var badge = document.createElement('span');
+var miLocalStorage = window.localStorage;
+badge.classList.add('position-absolute', 'top-0', 'start-100', 'translate-middle', 'badge', 'rounded-pill', 'bg-danger', 'carritoContador');
+
 const baseDeDatos = [
         {
             id: 1,
@@ -129,12 +141,12 @@ const baseDeDatos = [
         },
         {
             id: 11,
-            nombre: 'Doblemente Intenso',
-            precio: 299.98,
-            precioPromo: 279.99,
+            nombre: 'Doble Intenso',
+            precio: 279.99,
+            precioAnterior: 299.98,
             promoStatus: true,
             origen: 'Origen: Chiapas.',
-            sabor: 'Contiene 2 Yaxkin Molido de 500g cada uno.',
+            descripcion: 'Contiene 2 Yaxkin Molido de 500g cada uno.',
             contenido: '',
             categoria:'Paquete',
             imagen: '/src/promo1.jpg'
@@ -142,11 +154,11 @@ const baseDeDatos = [
         {
             id: 12,
             nombre: 'Mujer Coffee Kode',
-            precio: 279.98,
-            precioPromo: 249.99,
+            precio: 249.99,
+            precioAnterior: 279.98,
             promoStatus: true,
             origen: 'Origen: Tabasco.',
-            sabor: 'Contiene 1 Ixchel Molido y 1 de Grano de 500g cada uno.                        ',
+            descripcion: 'Contiene 1 Ixchel Molido y 1 de Grano de 500g cada uno.                        ',
             contenido: '',
             categoria:'Paquete',
             imagen: '/src/promo2.jpg'
@@ -154,11 +166,11 @@ const baseDeDatos = [
         {
             id: 13,
             nombre: 'Coffee Kode Plus',
-            precio: 389.97,
-            precioPromo: 349.99,
+            precio: 349.99,
+            precioAnterior: 389.97,
             promoStatus: true,
             origen: 'Origen: Veracruz.',
-            sabor: 'Contiene Amaité, Yatzil y Yaxkin de Grano de 500g.',
+            descripcion: 'Contiene Amaité, Yatzil y Yaxkin de Grano de 500g.',
             contenido: '',
             categoria:'Paquete',
             imagen: '/src/promo3.jpg'
@@ -166,11 +178,11 @@ const baseDeDatos = [
         {
             id: 14,
             nombre: 'Koffee Code Premium',
-            precio: 449.97,
-            precioPromo: 399.99,
+            precio: 399.99,
+            precioAnterior: 449.97,
             promoStatus: true,
             origen: 'Origen: Tabasco.',
-            sabor: 'Contiene Amaité, Yatzil y Yaxkin Molido de 500g.',
+            descripcion:'Contiene Amaité, Yatzil y Yaxkin Molido de 500g.',
             contenido: '',
             categoria:'Paquete',
             imagen: '/src/promo4.jpg'
@@ -178,11 +190,11 @@ const baseDeDatos = [
         {
             id: 15,
             nombre: 'Coffee Kode Ultimate',
-            precio: 649.95,
-            precioPromo: 549.99,
+            precio: 549.99,
+            precioAnterior: 649.95,
             promoStatus: true,
             origen: 'Origen: Chiapas.',
-            sabor: 'Contiene 5 bolsas de 500g de cada uno de nuestros Cafés de Grano.',
+            descripcion: 'Contiene 5 bolsas de 500g de cada uno de nuestros Cafés de Grano.',
             contenido: '',
             categoria:'Paquete',
             imagen: '/src/promo5.jpg'
@@ -195,7 +207,7 @@ function mostrarMolido() {
         categoriaMostrada.innerHTML = "MOLIDO";
         DOMitems.removeChild(DOMcatalogo);
         DOMcatalogo = document.createElement('div');
-        DOMcatalogo.classList.add('row', 'row-cols-1', 'row-cols-sm-2', 'row-cols-md-3', 'row-cols-lg-4', 'g-4', 'listaProductos');
+        DOMcatalogo.classList.add('row', 'row-cols-1', 'row-cols-sm-2', 'row-cols-md-3', 'row-cols-lg-3', 'g-4', 'listaProductos');
         DOMitems.appendChild(DOMcatalogo);
         baseDeDatos.forEach(
             (producto) => {
@@ -242,7 +254,9 @@ function mostrarMolido() {
             miNodoBoton.classList.add('storeButton1', 'agregar-carrito');
             miNodoBoton.textContent = 'AÑADIR +';
             miNodoBoton.setAttribute('data-id', producto.id);
-            miNodoBoton.addEventListener('click', agregarCarrito);
+            miNodoBoton.addEventListener('click',  añadirProductoAlCarrito);
+            miNodoBoton.addEventListener('click', contadorCarrito);
+            
 
             // Insertamos
             DOMcatalogo.appendChild(miNodo);
@@ -259,7 +273,7 @@ function mostrarMolido() {
             miNodoLista.appendChild(miNodoListaSabor);
             miNodoLista.appendChild(miNodoListaContenido);
 
-            console.log(producto.id+producto.categoria);
+            console.log(producto.id);
         }
     });
         
@@ -270,7 +284,7 @@ function mostrarGrano(){
     categoriaMostrada.innerHTML = "GRANO";
             DOMitems.removeChild(DOMcatalogo);
         DOMcatalogo = document.createElement('div');
-        DOMcatalogo.classList.add('row', 'row-cols-1', 'row-cols-sm-2', 'row-cols-md-3', 'row-cols-lg-4', 'g-4', 'listaProductos');
+        DOMcatalogo.classList.add('row', 'row-cols-1', 'row-cols-sm-2', 'row-cols-md-3', 'row-cols-lg-3', 'g-4', 'listaProductos');
         DOMitems.appendChild(DOMcatalogo);
         baseDeDatos.forEach(
             (producto) => {
@@ -317,7 +331,8 @@ function mostrarGrano(){
             miNodoBoton.classList.add('storeButton1', 'agregar-carrito');
             miNodoBoton.textContent = 'AÑADIR +';
             miNodoBoton.setAttribute('data-id', producto.id);
-            miNodoBoton.addEventListener('click', agregarCarrito);
+            miNodoBoton.addEventListener('click', añadirProductoAlCarrito);
+            miNodoBoton.addEventListener('click', contadorCarrito);
 
             // Insertamos
             DOMcatalogo.appendChild(miNodo);
@@ -334,7 +349,7 @@ function mostrarGrano(){
             miNodoLista.appendChild(miNodoListaSabor);
             miNodoLista.appendChild(miNodoListaContenido);
 
-            console.log(producto.id+producto.categoria);
+            console.log(producto.id);
         }
     });
         
@@ -344,7 +359,7 @@ function mostrarPromociones(){
     categoriaMostrada.innerHTML = "PROMOCIONES";
         DOMitems.removeChild(DOMcatalogo);
         DOMcatalogo = document.createElement('div');
-        DOMcatalogo.classList.add('row', 'row-cols-1', 'row-cols-sm-2', 'row-cols-md-3', 'row-cols-lg-4', 'g-4', 'listaProductos');
+        DOMcatalogo.classList.add('row', 'row-cols-1', 'row-cols-sm-2', 'row-cols-md-3', 'row-cols-lg-3', 'g-4', 'listaProductos');
         DOMitems.appendChild(DOMcatalogo);
         baseDeDatos.forEach(
             (producto) => {
@@ -375,10 +390,10 @@ function mostrarPromociones(){
             miNodoLista.classList.add('list-unstyled', 'mt-3', 'mb-4');
             // Items en lista de Card-Body
             let miNodoListaPrecioAnterior = document.createElement('li');
-            miNodoListaPrecioAnterior.textContent = producto.precio;
+            miNodoListaPrecioAnterior.textContent = producto.precioAnterior;
             miNodoListaPrecioAnterior.classList.add('precioAnterior');
-            let miNodoListaSabor = document.createElement('li');
-            miNodoListaSabor.textContent = producto.sabor;
+            let miNodoListaDescripcion = document.createElement('li');
+            miNodoListaDescripcion.textContent = producto.descripcion;
             let miNodoListaContenido = document.createElement('li');
             miNodoListaContenido.textContent = producto.contenido;
             // Precio
@@ -386,13 +401,15 @@ function mostrarPromociones(){
             miNodoPrecio.classList.add('pricing-card-title', 'precio');
             miNodoPrecio.textContent = '$';
             let miNodoSpan = document.createElement('span');
-            miNodoSpan.textContent = producto.precioPromo;
+            miNodoSpan.textContent = producto.precio;
             // Boton 
             let miNodoBoton = document.createElement('button');
             miNodoBoton.classList.add('storeButton1', 'agregar-carrito');
             miNodoBoton.textContent = 'AÑADIR +';
             miNodoBoton.setAttribute('data-id', producto.id);
-            miNodoBoton.addEventListener('click', agregarCarrito);
+            miNodoBoton.addEventListener('click',  añadirProductoAlCarrito);
+            miNodoBoton.addEventListener('click', contadorCarrito);
+           
 
             // Insertamos
             DOMcatalogo.appendChild(miNodo);
@@ -406,31 +423,163 @@ function mostrarPromociones(){
             miNodoCardBody.appendChild(miNodoBoton);   
             miNodoPrecio.appendChild(miNodoSpan);
             miNodoLista.appendChild(miNodoListaPrecioAnterior);
-            miNodoLista.appendChild(miNodoListaSabor);
+            miNodoLista.appendChild(miNodoListaDescripcion);
             miNodoLista.appendChild(miNodoListaContenido);
 
-            console.log(producto.id+producto.categoria);
+            console.log(producto.id);
         }
     });
         
 }
 
-let badge = document.createElement('span');
-badge.classList.add('position-absolute', 'top-0', 'start-100', 'translate-middle', 'badge', 'rounded-pill', 'bg-danger', 'carritoContador');
 
 
-function agregarCarrito(){
+// FUNCION PARA CONTADOR DEL CARRITO
+function contadorCarrito(){
     carritoBadge.appendChild(badge);
     carritoBadge.removeChild(badge);
     badge = document.createElement('span');
-    badge.classList.add('position-absolute', 'top-0', 'start-100', 'translate-middle', 'badge', 'rounded-pill', 'bg-danger', 'carritoContador');
-    badge.textContent = contadorItems;
-    carritoBadge.appendChild(badge);
-    contadorItems++; 
+    badge.classList.add('position-absolute', 'start-100', 'translate-middle', 'badge', 'rounded-pill', 'bg-success', 'carritoContador');
+    badge.textContent = carrito.length;
+    carritoBadge.appendChild(badge); 
+    if (carrito.length == 0){
+        carritoBadge.removeChild(badge);
+    }
 }
-var contadorItems = 1;
-mostrarMolido();
 
+
+function añadirProductoAlCarrito(evento) {
+    // Anyadimos el Nodo a nuestro carrito
+    carrito.push(evento.target.getAttribute('data-id'))
+    console.log('Se añadió el producto con el id: ', evento.target.getAttribute('data-id'));
+    // Actualizamos el carrito 
+    mostrarCarrito();
+    guardarCarritoEnLocalStorage();
+
+}
+
+function mostrarCarrito() {
+    // Vaciamos todo el html
+    DOMcarrito.textContent = '';
+    // Quitamos los duplicados
+    const carritoSinDuplicados = [...new Set(carrito)];
+    // Generamos los Nodos a partir de carrito
+    carritoSinDuplicados.forEach((item) => {
+        // Obtenemos el item que necesitamos de la variable base de datos
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+        // ¿Coincide las id? Solo puede existir un caso
+        return itemBaseDatos.id === parseInt(item);
+        });
+        // Cuenta el número de veces que se repite el producto
+        const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+            // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+            return itemId === item ? total += 1 : total;
+        }, 0);
+        // Creamos el nodo enlistar el producto en cuerpo del modal      
+        const miNodo = document.createElement('li');
+        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+        const miRow = document.createElement('div');
+        miRow.classList.add('row', 'align-items-center', 'justify-content-evenly');
+        const miCol1 = document.createElement('div');
+        miCol1.classList.add('col', 'align-items-center', 'columna1');
+        miCol1.textContent = `${numeroUnidadesItem}x`;
+        const miCol2 = document.createElement('div');
+        miCol2.classList.add('col','align-items-center', 'columna2');
+        const miImagen = document.createElement('img');
+        miImagen.setAttribute('src', miItem[0].imagen);
+        miImagen.setAttribute('width', '60px');
+        const miCol3 = document.createElement('div');
+        miCol3.classList.add('col','align-items-center', 'columna3');
+        miCol3.textContent = `${miItem[0].nombre}`;
+        const miCol4 = document.createElement('div');
+        miCol4.classList.add('col','align-items-center', 'columna4');
+        miCol4.textContent = `${divisa}${miItem[0].precio}`;
+        const miCol5 = document.createElement('div');
+        miCol5.classList.add('col','align-items-center', 'columna5');
+        
+        // Boton de borrar
+        const miBoton = document.createElement('button');
+        miBoton.classList.add('btn', 'btn-success', 'quitarButton');
+        miBoton.textContent = 'X';
+        miBoton.style.marginLeft = '1rem';
+        miBoton.dataset.item = item;
+        miBoton.addEventListener('click', borrarItemCarrito);
+        // Mezclamos nodos
+        miNodo.appendChild(miRow);
+        miRow.appendChild(miCol1);
+        miRow.appendChild(miCol2);
+        miRow.appendChild(miCol3);
+        miRow.appendChild(miCol4);
+        miRow.appendChild(miCol5);
+        miCol5.appendChild(miBoton);
+        miCol2.appendChild(miImagen);
+        DOMcarrito.appendChild(miNodo);
+    });
+    // Renderizamos el precio total en el HTML
+    DOMtotal.textContent = calcularTotal();
+    DOMtotalArticulos.innerHTML = carrito.length;
+    statusCarrito.innerHTML = carrito.length == 0 ? "Tu carrito está vacío." : "Resumen:";
+}
+
+function borrarItemCarrito(evento) {
+    // Obtenemos el producto ID que hay en el boton pulsado
+    const id = evento.target.dataset.item;
+    // Borramos todos los productos
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== id;
+    });
+    // volvemos a renderizar
+    mostrarCarrito();
+    contadorCarrito();
+    guardarCarritoEnLocalStorage();
+
+}
+
+function calcularTotal() {
+    // Recorremos el array del carrito 
+    return carrito.reduce((total, item) => {
+        // De cada elemento obtenemos su precio
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        // Los sumamos al total
+        return total + miItem[0].precio;
+    }, 0).toFixed(2);
+}
+
+function vaciarCarrito() {
+    // Limpiamos los productos guardados
+    carrito = [];
+    // Renderizamos los cambios
+    mostrarCarrito();
+    contadorCarrito();
+    localStorage.clear();
+    console.log('Se eliminó carrito del local storage.');
+}
+
+function guardarCarritoEnLocalStorage () {
+    miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+    console.log('Se guardó carrito en el local storage.');
+}
+
+function cargarCarritoDeLocalStorage () {
+    // ¿Existe un carrito previo guardado en LocalStorage?
+    if (miLocalStorage.getItem('carrito') !== null) {
+        // Carga la información
+        carrito = JSON.parse(miLocalStorage.getItem('carrito'));
+        console.log('Se cargó carrito del local storage.');
+    }
+}
+
+// 
+mostrarMolido();
+cargarCarritoDeLocalStorage();
+mostrarCarrito();
+contadorCarrito();
+
+
+// EVENTOS DE BOTONES DE CATEGORIAS
 buttonMolido.addEventListener("click", mostrarMolido);
 buttonGrano.addEventListener("click", mostrarGrano);
 buttonPromociones.addEventListener("click", mostrarPromociones);
+DOMbotonVaciar.addEventListener('click', vaciarCarrito);
