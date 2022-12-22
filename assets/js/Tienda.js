d5a7eb83d64fab4101405b4691094519c0803376
DeshiH <rodrickvkjr@gmail.com>
2022-12-22 15:13:23 -0600

    yaa

--- assets/js/Tienda.js
+++ assets/js/Tienda.js
@@ -30,7 +30,7 @@ const baseDeDatos = [
             sabor: 'Sabor: Intenso-suave.',
             contenido: 'Contenido: 500g.',
             categoria:'Molido',
-            imagen: '../img/Yatzil-molido.jpg'
+            imagen: '/assets/img/Yatzil-molido.jpg'
         },
         {
             id: 2,
